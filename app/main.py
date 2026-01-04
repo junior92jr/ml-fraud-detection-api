a62f311e6577a4414d360c6aec7878fddb838c53
nataliia <ballad.of.a.muse@gmail.com>
2026-01-04 15:58:19 +0000

    model route added
    	modified:   app/main.py

--- app/main.py
+++ app/main.py
@@ -1,12 +1,16 @@
+import os
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 
 from app.database import create_db_and_tables
-from app.routers import score, transactions
+from app.inference.predictor import Predictor
+from app.routers import predict, score, transactions
 from app.utils.logger import logger_config
 
 logger = logger_config(__name__)
+
+MODEL_PATH = os.getenv("MODEL_PATH", "artifacts/model.joblib")
 
 
 @asynccontextmanager
@@ -16,6 +20,15 @@ async def lifespan(app: FastAPI):
     create_db_and_tables()
 
     logger.info("startup: triggered")
+
+    # ML model init
+    predictor = Predictor(MODEL_PATH)
+    predictor.load()
+
+    # Store predictor on app state
+    app.state.predictor = predictor
+
+    logger.info("startup: DB initialized, ML model loaded")
 
     yield
 
@@ -33,6 +46,11 @@ def create_application() -> FastAPI:
     application.include_router(score.router, prefix="/score", tags=["Scoring"])
     application.include_router(
         transactions.router, prefix="/transactions", tags=["Transactions"]
+    )
+
+    application.include_router(
+        predict.router,
+        tags=["Prediction"],
     )
 
     return application
